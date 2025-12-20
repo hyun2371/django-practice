b25da9dbdb440f856041903c086b70ec470e1bc1
hs12 <loveanna1228@gmail.com>
2025-12-20 22:35:24 +0900

    feat: User 모델 확장

--- ch3/auth_user/user/models.py
+++ ch3/auth_user/user/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+class CustomUser(AbstractUser):
+    social_provider = models.CharField(max_length=8, null=True)
 
 # Create your models here.
