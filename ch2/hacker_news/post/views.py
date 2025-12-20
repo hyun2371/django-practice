bfef01e99bdff179f47316040c2ed3623f1a7965
hs12 <loveanna1228@gmail.com>
2025-12-20 15:48:22 +0900

    refactor: class-based view

--- ch2/hacker_news/post/views.py
+++ ch2/hacker_news/post/views.py
@@ -1,28 +1,31 @@
 from gc import get_objects
 
 from django.db.models import F
+from django.views import View
 from django.http import HttpResponse, request, HttpResponseBadRequest
 from django.shortcuts import render, get_object_or_404, redirect
 from post.models import Post
 from post.forms import PostForm
+
+
 # Create your views here.
 
-def posts_view(request):
-    if request.method == "GET":
+class PostListView(View):
+    def get(self, request):
         posts = Post.objects.all()
-        context = {"posts" : posts, "form": PostForm}
+        context = {"posts": posts, "form": PostForm}
         return render(
             request, "post_list.html", context
         )
 
-
-def post_create_view(request):
-    if request.method == "GET":
+class PostCreateView(View):
+    def get(self, request):
         context = {"form": PostForm}
         return render(
             request, "post_create.html", context
         )
-    elif request.method == "POST":
+
+    def post(self, request):
         form = PostForm(request.POST)
         if form.is_valid():  # 유효성 검사 통과
             title = form.cleaned_data["title"]
@@ -40,19 +43,20 @@ def post_create_view(request):
 
         return redirect("posts")  # 엔드포인트
 
+class PostDetailView(View):
+    def get(self, request, post_id):
+        post = get_object_or_404(Post, id=post_id)
+        context = {"post": post}  # html key 변수와 동일해야 함
+        return render(
+            request, "post_detail.html", context
+        )
 
-def post_view(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    context = {"post" : post} #html key 변수와 동일해야 함
-    return render(
-        request, "post_detail.html", context
-    )
-
-def post_like_view(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    post.points = F("points") + 1 # 원자적 연산
-    post.save()
-    post.refresh_from_db()
-    return render(
-        request, "post_detail.html", {"post": post}
-    )
+class PostLikeView(View):
+    def post(self,request, post_id):
+        post = get_object_or_404(Post, id=post_id)
+        post.points = F("points") + 1  # 원자적 연산
+        post.save()
+        post.refresh_from_db()
+        return render(
+            request, "post_detail.html", {"post": post}
+        )
