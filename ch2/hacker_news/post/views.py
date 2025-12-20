0f43be60befa93fd81b8276e649094c23cc93dda
hs12 <loveanna1228@gmail.com>
2025-12-20 13:43:30 +0900

    feat: form 입력

--- ch2/hacker_news/post/views.py
+++ ch2/hacker_news/post/views.py
@@ -1,16 +1,37 @@
 from gc import get_objects
 
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, request, HttpResponseBadRequest
+from django.shortcuts import render, get_object_or_404, redirect
 from post.models import Post
+from post.forms import PostForm
 # Create your views here.
 
 def posts_view(request):
-    posts = Post.objects.all()
-    context = {"posts" : posts}
-    return render(
-        request, "post_list.html", context
-    )
+    if request.method == "GET":
+        posts = Post.objects.all()
+        context = {"posts" : posts, "form": PostForm}
+        return render(
+            request, "post_list.html", context
+        )
+    elif request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid(): #유효성 검사 통과
+            title = form.cleaned_data["title"]
+            body = form.cleaned_data["body"]
+            author_name = form.cleaned_data["author_name"]
+
+            post = Post.objects.create(
+            title=title,body=body,author_name=author_name
+            )
+            context = {"post" : post, "form": PostForm} #context -> 뷰에서 만든 데이터를 html로 전달
+
+            return render(
+                request, "post_detail.html", context
+            )
+
+        return redirect("posts") # 엔드포인트
+
+
 
 def post_view(request, post_id):
     post = get_object_or_404(Post, id=post_id)
