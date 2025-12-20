170b93862b585e3404385ada3d00f8211af0a696
hs12 <loveanna1228@gmail.com>
2025-12-20 19:36:09 +0900

    feat: 로그인

--- ch3/auth_user/config/settings.py
+++ ch3/auth_user/config/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "user.apps.UserConfig",
 ]
 
 MIDDLEWARE = [
@@ -115,3 +115,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_REDIRECT_URL = 'home'
---
+++ ch3/auth_user/user/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.views import View
+
+class HomeView(View):
+    def get(self, request):
+        # context = {"user" : user, "perms": perms}
+        return render(request, "home.html")
