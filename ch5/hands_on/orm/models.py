170aa1862fccded9d85e1a4a01b589a767643ed1
hs12 <loveanna1228@gmail.com>
2025-12-21 18:05:05 +0900

    [ORM] 다대일 관계

--- ch5/hands_on/orm/models.py
+++ ch5/hands_on/orm/models.py
@@ -4,10 +4,16 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     # profile = models.OneToOneField() 역참조 필드 생성됨
+    # user.order_set.all() 일대다 조회
     pass
+
 
 class Profile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
 
-# user1 -> profile1(on_delete=CASCADE) 유저1 삭제 시 프로필1도 삭제됨
+
+class Order(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    price = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
