706319f3e32927495557a05dc636352c968779e6
hs12 <loveanna1228@gmail.com>
2025-12-21 11:32:12 +0900

    [short_url] 홈화면 구현

--- ch4/short_url/shortener/views.py
+++ ch4/short_url/shortener/views.py
@@ -1,3 +1,14 @@
+from django.views import View
 from django.shortcuts import render
 
-# Create your views here.
+from shortener.models import ShortURL
+
+
+class HomeView(View):
+    def get(self, request):
+        short_urls = ShortURL.objects.all() # shortUrlRepository.findAll();
+        return render(
+            request, "home.html",
+            {"short_urls":short_urls}
+        )
+
