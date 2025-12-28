15ff8256bcfda3617e0d266ace69d45fe7005a19
hs12 <loveanna1228@gmail.com>
2025-12-28 20:22:27 +0900

    [내장기능] html 이메일 전송

--- ch5/hands_on/utils.py
+++ ch5/hands_on/utils.py
@@ -1,5 +1,7 @@
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMessage
 from django.conf import settings
+from django.template.loader import render_to_string
+
 
 def send_str_email(email_to):
     send_mail(
@@ -8,3 +10,15 @@ def send_str_email(email_to):
         settings.EMAIL_HOST_USER,
         [email_to],
     )
+
+
+def send_html_email(email_to):
+    subject = "새로운 이메일이 도착했습니다."
+    html = render_to_string("email.html")
+    message = EmailMessage(
+        subject, html,
+        settings.EMAIL_HOST_USER,
+        [email_to]
+    )
+    message.content_subtype = "html"
+    message.send()
