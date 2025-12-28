015f1b3edc65b49470d7a4f3dacb4e8d665fc32f
hs12 <loveanna1228@gmail.com>
2025-12-28 19:48:13 +0900

    [ORM] DB엔진 변경

--- ch5/hands_on/config/settings.py
+++ ch5/hands_on/config/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hands_on',
+        'USER': 'dev_user',
+        'PASSWORD': 'securepassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
