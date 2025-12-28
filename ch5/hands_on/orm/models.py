2e066c4f91a52d824005611e799559d136df4744
hs12 <loveanna1228@gmail.com>
2025-12-28 17:40:41 +0900

    [ORM] Django Managers

--- ch5/hands_on/orm/models.py
+++ ch5/hands_on/orm/models.py
@@ -2,6 +2,10 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 import uuid
 
+class CustomUserManager(models.Manager):
+    def with_image(self): # 자주 사용되는 쿼리 패턴을 함수로 정의
+        return super().get_queryset().filter(image__isnull=False) #이미지가 존재하는 사용자만 조회
+
 class CustomUser(AbstractUser):
     uuid = models.UUIDField(
         default=uuid.uuid4, editable=False, db_index=True
@@ -11,4 +15,6 @@ class CustomUser(AbstractUser):
         upload_to="profiles/",
         null=True,
     )
+    preferences = models.JSONField(default=dict)
 
+    objects = CustomUserManager()
